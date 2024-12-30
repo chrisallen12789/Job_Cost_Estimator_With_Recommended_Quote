1106da39d7093e222d0ad309479420ef9db2446c
Christopher Lane <christopher.lane0127@gmail.com>
2024-12-30 17:14:54 -0500

    working

--- total_disposal_cost.py
+++ total_disposal_cost.py
@@ -17,23 +17,26 @@ def disposal_rate():
     inkster_total_expense = 0
     dearborn_total_min_expense = 0
     dearborn_total_expense = 0
+    while disposal_location == "":
+        try:
+            if disposal_location == ["neither", "no", "none"]:
+                inkster_total_expense = 0
 
-    if disposal_location == ["neither", "no", "none"]:
-        inkster_total_expense = 0
-
-    if disposal_location == "inkster":
-        if disposal_rate_expectation <= 2:
-            inkster_total_min_expense = minimal_disposal_fee + dump_travel_cost
-        elif disposal_rate_expectation > 2:
-            inkster_total_expense = (valicor_inkster_disposal * disposal_rate_expectation) * (1 + environmental_fee) + dump_travel_cost
-        else:
-            inkster_total_expense = 0
-    if disposal_location == "dearborn":
-        if disposal_rate_expectation <= 2:
-            dearborn_total_min_expense = minimal_disposal_fee + dump_travel_cost
-        elif disposal_rate_expectation > 2:
-            dearborn_total_expense = (valicor_dearborn_disposal * disposal_rate_expectation) * (1 + environmental_fee) + dump_travel_cost
-        else:
-            dearborn_total_expense = 0
+            if disposal_location == "inkster":
+                if disposal_rate_expectation <= 2:
+                    inkster_total_min_expense = minimal_disposal_fee + dump_travel_cost
+                elif disposal_rate_expectation > 2:
+                    inkster_total_expense = (valicor_inkster_disposal * disposal_rate_expectation) * (1 + environmental_fee) + dump_travel_cost
+                else:
+                    inkster_total_expense = 0
+            if disposal_location == "dearborn":
+                if disposal_rate_expectation <= 2:
+                    dearborn_total_min_expense = minimal_disposal_fee + dump_travel_cost
+                elif disposal_rate_expectation > 2:
+                    dearborn_total_expense = (valicor_dearborn_disposal * disposal_rate_expectation) * (1 + environmental_fee) + dump_travel_cost
+                else:
+                    dearborn_total_expense = 0
+        except ValueError:
+            print("You need to enter Inkster, Dearborn, or None for shop disposal.")
     disposal_total_expense = inkster_total_min_expense + inkster_total_expense + dearborn_total_min_expense + dearborn_total_expense
     return disposal_total_expense
